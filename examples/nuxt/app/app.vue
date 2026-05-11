c5bb5fd47fd4d00b76e73fe0cbddacbf219e171b
Teages <Teages@teages.xyz>
2026-05-11 04:10:13 +0000

    chore: add nuxt example

---
+++ examples/nuxt/app/app.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { MediaContentFragment } from '~/components/MediaContent.vue';
+
+const query = schema.query('FetchAnime')
+  .vars({ id: 'Int = 127549' })
+  .select(($, vars) => $.select([{
+    Media: $ => $.args({ id: vars.id, type: schema.enum('ANIME') })
+      .select([
+        'id',
+        ...MediaContentFragment(vars),
+      ]),
+  }]))
+
+const { data, error } = useAsyncData(() => client.request(query, { }))
+</script>
+
+<template>
+  <div>
+    Media:
+    <div v-if="error">{{ error.message }}</div>
+    <MediaContent v-else-if="data?.Media" :media="data.Media" />
+    <div v-else>
+      Loading...
+    </div>
+  </div>
+</template>
---
+++ examples/nuxt/app/components/MediaContent.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+export const MediaContentFragment = schema.partial('MediaContent_Media')
+  .on('Media')
+  .select($ => $.select([
+    'id',
+    {
+      title: $ => $.select([
+        'romaji',
+        'english',
+        'native',
+      ]),
+    },
+  ]))
+
+</script>
+
+<script setup lang="ts">
+const { media: mediaRaw } = defineProps<{
+  media: FragmentOf<typeof MediaContentFragment>
+}>()
+
+const media = computed(() => readFragment(MediaContentFragment, mediaRaw))
+</script>
+
+<template>
+  <div>
+    {{ media }}
+  </div>
+</template>
