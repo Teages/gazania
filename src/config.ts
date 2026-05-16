453e36f4cd81fb45bc15175cd76cc42e1833b611
Teages <Teages@teages.xyz>
2026-05-16 22:20:05 +0800

    feat(config): export SchemaConfig and ExtractConfig types from gazania/config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export { defineConfig } from './cli/config'
-export type { Config, GetterSource, JsonSource, SchemaLoader, SdlSource, UrlSource, UserConfig } from './cli/config'
+export type { Config, ExtractConfig, GetterSource, JsonSource, SchemaConfig, SchemaLoader, SdlSource, UrlSource, UserConfig } from './cli/config'
