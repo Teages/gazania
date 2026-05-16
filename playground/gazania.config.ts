779dedcdee6ed766bfc1e033e26621fcd8f13c6d
Teages <Teages@teages.xyz>
2026-05-16 22:26:16 +0800

    chore(playground): migrate config to new schemas array format

--- playground/gazania.config.ts
+++ playground/gazania.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'gazania/config'
 
 export default defineConfig({
-  output: 'gazania/anilist.ts',
-  schema: 'https://graphql.anilist.co',
+  schemas: [
+    {
+      output: 'gazania/anilist.ts',
+      schema: 'https://graphql.anilist.co',
+    },
+  ],
 })
